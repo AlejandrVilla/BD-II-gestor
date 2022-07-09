5d592005f609e177a28b0bcb4a9b3388737b564d
villas12villas34 <fvillarroel@unsa.edu.pe>
2022-07-08 19:24:52 -0500

    b+tree

--- B+tree/main.cpp
+++ B+tree/main.cpp
@@ -9,14 +9,13 @@ int main(){
     string mensaje = "mensaje";
     int orden = 3;
 
-    cout << "\nPrograma que almacena numeros en una estructura arbol B+ de orden 3.\n\n";
 
-    cout << "\nSe cargan los datos que agregar al arbol desde un archivo de texto guardado en la carpeta 'valores', este no debe incluir .txt, la ruta ya esta definida.\n \
-        Para eliminar datos, de igual manera se cargan en desde la carpeta 'valoresBorrados', y se eliminan los valores que contenga este archivo\n" << endl;
+    cout << "\nLos tests de cargar valores se encuentran en 'casos' \n \
+        Los tests para eliminar valores se encuentran en 'casosDeBorrado'\n" << endl;
 
     Arbol A(orden);
 
-    cout << "\n\nIngrese el nombre del archivo con valores para\nagregar al arbol, en la carpeta valores:  ";
+    cout << "\n\nIngrese el nombre del archivo con valores para\nagregar al arbol, en la carpeta 'casos':  ";
     cin >> mensaje;
 
     A.lecturaLlaves(mensaje);
@@ -28,7 +27,7 @@ int main(){
 
     cin >> mensaje;
     if(mensaje == "1"){
-        cout << "\n\nIngrese el nombre del archivo con valores para\nborrar del arbol, en la carpeta valoresBorrados:  ";
+        cout << "\n\nIngrese el nombre del archivo con valores para\nborrar del arbol, en la carpeta casosDeBorrados:  ";
         cin >> mensaje;
 
         A.borradoLlaves(mensaje);
@@ -36,7 +35,6 @@ int main(){
         A.pintar();
     }
 
-    cout << "\nEspinoza Maciel Gilberto \n Paredes Padilla Jorge Xavier \n Figueroa Miranda Jose Jesus \n\n";
 
     system("pause");
     return 0;
