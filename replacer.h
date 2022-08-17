c26526f80a359656baeb332054ae1037f4c7dfd5
MJSoto123 <68660162+MJSoto123@users.noreply.github.com>
2022-08-17 00:52:30 -0500

    replacer rdy

--- replacer.h
+++ replacer.h
@@ -6,10 +6,11 @@
 class Replacer
 {
     public:
+        // Define los metodos virtuales
         Replacer() = default;
         virtual ~Replacer() = default;
-        virtual bool Victim(int32_t *frame_id) = 0;
-        virtual void Pin(int32_t frame_id) = 0;
-        virtual void Unpin(int32_t frame_id) = 0;
-        virtual size_t Size() = 0;
+        virtual bool Victim(int32_t *frame_id) = 0;     // Metodo de eleccion de victima
+        virtual void Pin(int32_t frame_id) = 0;         // Pin frame 
+        virtual void Unpin(int32_t frame_id) = 0;       // Unpin frame
+        virtual size_t Size() = 0;                      // tamanio de buffer
 };
